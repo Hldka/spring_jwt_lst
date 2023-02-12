40364e17fcbb62e7b4857d2d3aa2e0e74b81e1c0
Hldka <116027001+Hldka@users.noreply.github.com>
2023-02-12 17:02:50 +0100

    first commit

--- src/main/java/com/tpe/security/AuthTokenFilter.java
+++ src/main/java/com/tpe/security/AuthTokenFilter.java
@@ -1,4 +1,69 @@
 package com.tpe.security;
 
-public class AuthTokenFilter {
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.context.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Component;
+import org.springframework.util.*;
+import org.springframework.web.filter.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String jwtToken = parseJwt(request);
+
+        try {
+            if(jwtToken!=null && jwtUtils.validateToken(jwtToken)){
+
+                String userName  = jwtUtils.getUserNameFromJwtToken(jwtToken);// securuty contex'e atabilmek icin
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+
+                UsernamePasswordAuthenticationToken authentication =// springboot bize veriyor
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);//security contex'e göndermek icin
+
+            }
+        } catch (UsernameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        filterChain.doFilter(request,response);
+
+
+    }
+
+    private String parseJwt(HttpServletRequest request){
+        String header =  request.getHeader("Authorization");
+        if(StringUtils.hasText(header) && header.startsWith("Bearer ")) {// value degerin icinde ifade varmi ?ve ne ile basladigini
+            return header.substring(7);
+        }
+        return null;
+
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        return antPathMatcher.match("/register", request.getServletPath()) ||// kullanici securuty katmanina girmeden buraya girebilsin diye yaptik
+                antPathMatcher.match("/login" , request.getServletPath());
+    }
 }
--- src/main/java/com/tpe/security/JwtUtils.java
+++ src/main/java/com/tpe/security/JwtUtils.java
@@ -1,4 +1,62 @@
 package com.tpe.security;
 
+import com.tpe.security.service.*;
+import io.jsonwebtoken.*;
+import org.springframework.security.core.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
+@Component
 public class JwtUtils {
+    // 1 : JWT generate
+    // 2: JWT valide
+    // 3 : JWT --> userName
+    private String jwtSecret = "sboot";// istedigimiz seyi yazabiliriz
+
+    private  long jwtExpirationMs = 86400000;   // 24*60*60*1000 birgün diyeceksek
+
+    // !!! ************ GENERATE TOKEN *****************
+    public String generateToken(Authentication authentication) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();// anlik olarak login islemi gerceklesen user'a ulastim
+
+        return Jwts.builder().// jw tokene'i üretir
+                setSubject(userDetails.getUsername()).//jwtokeniniz username'den üretilir
+                setIssuedAt(new Date()).// jwtoken ne zaman üretildi
+                setExpiration(new Date(new Date().getTime() + jwtExpirationMs)).// jwt tokeni ne zaman bitecek(new ne zama olacagini ve 24 saat olacagini ekeldeim)
+                signWith(SignatureAlgorithm.HS512, jwtSecret).// sifreleme algoritmalari
+                compact();
+    }
+
+    // !!! ****************** VALIDATE TOKEN ***************************
+    public boolean validateToken(String token){
+
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            e.printStackTrace();
+        } catch (UnsupportedJwtException e) {
+            e.printStackTrace();
+        } catch (MalformedJwtException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        return false ;
+    }
+
+    // !!! ********** JWT tokenden userName'i alalım ************
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().//
+                setSigningKey(jwtSecret).// ne ile sifreledim
+                parseClaimsJws(token).
+                getBody().
+                getSubject();
+        // ya password ile username ie yapabilirim ,user name uniq onun icin ulasmam daha kolay
+        //filter kisminda jwt generate ve jwt validate yaptik
+    }
+
 }
